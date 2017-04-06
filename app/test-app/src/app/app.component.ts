65f6090360c60a85874e5b7b989d2209be55776c
zhaosiyang <kern_zhao@126.com>
2017-04-06 18:18:13 -0400

    wip

--- app/test-app/src/app/app.component.ts
+++ app/test-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import * as io from 'socket.io-client';
 import * as io from 'socket.io-client';
 
 @Component({
@@ -10,13 +9,30 @@ import * as io from 'socket.io-client';
 export class AppComponent {
   private title = 'app works!';
   private messages: Array<string> = ['test1', 'test2'];
-  private serverUrl = 'http://localhost:3000';
+  private serverDomain = 'http://54.213.212.103:3000';
   private socket;
 
   ngOnInit() {
-    this.socket = io(this.serverUrl);
-    console.log('hello');
-    this.socket.on('message', data => this.messages.push(data));
+    // const tableName = 'MusicLibraryTest';
+    // const serverUrl = `${this.serverDomain}/${tableName}`;
+    // console.log(serverUrl);
+    // this.socket = io(serverUrl);
+    //
+    // this.socket.on('message', data => {
+    //   console.log('got info');
+    //   this.messages.push(data);
+    // });
+
+
+    var socket = io('http://54.213.212.103:3000');
+    socket.on('connect', function(){
+      console.log('connected');
+    });
+    socket.on('message', function(data){
+      console.log('got message');
+    });
+    socket.on('disconnect', function(){});
+
   }
 
 }
