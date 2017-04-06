6e42e67d58b0928effd182ba7db17d22cba249fe
zhaosiyang <kern_zhao@126.com>
2017-04-05 22:23:03 -0400

    works for auto incremental numbers

--- app/test-app/src/app/app.component.ts
+++ app/test-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as io from 'socket.io-client';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private title = 'app works!';
+  private messages: Array<string> = ['test1', 'test2'];
+  private serverUrl = 'http://localhost:3000';
+  private socket;
+
+  ngOnInit() {
+    this.socket = io(this.serverUrl);
+    console.log('hello');
+    this.socket.on('message', data => this.messages.push(data));
+  }
+
 }
