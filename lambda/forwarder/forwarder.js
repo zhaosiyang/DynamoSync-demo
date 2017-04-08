47af44703b8f42a2c1be2a5c3def6fa7582fec4d
zhaosiyang <kern_zhao@126.com>
2017-04-07 20:50:09 -0400

    update

--- lambda/forwarder/forwarder.js
+++ lambda/forwarder/forwarder.js
@@ -1,7 +1,8 @@
 const request = require('request');
 
 exports.handler = function(e, ctx, cb) {
-  request(process.env.URL, {
+  e.tableName = process.env.TABLE_NAME;
+  request(`${process.env.URL}/__DYNAMO_SYNC__`, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify(e)
