0a85aebf8ba0f74811f80d762331ee34a6fc668c
zhaosiyang <kern_zhao@126.com>
2017-04-10 22:16:47 -0400

    setup

---
+++ app/test-app/src/modules/chats/chats.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'chats',
+    templateUrl: 'chats.component.html',
+    styleUrls: ['chats.component.css']
+})
+
+export class ChatsComponent {
+    title = 'Chats';
+}
---
+++ app/test-app/src/modules/shoppingList/shoppingList.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {ShoppingListComponent} from "./shoppingLIst.component";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    exports: [
+        ShoppingListComponent
+    ],
+    declarations: [
+        ShoppingListComponent
+    ]
+})
+export class ShoppingListModule {
+}
